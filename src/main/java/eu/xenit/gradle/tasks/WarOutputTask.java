e458b25dacab3b3e2cb88fe5bc7a03a27e926e5f
Lars Vierbergen <lars.vierbergen@xenit.eu>
2018-11-20 15:24:10 +0100

    Make MergeWarTask extend the Zip task and remove ability to set output war

--- src/main/java/eu/xenit/gradle/tasks/WarOutputTask.java
+++ src/main/java/eu/xenit/gradle/tasks/WarOutputTask.java
@@ -1,24 +1,11 @@
 package eu.xenit.gradle.tasks;
 
-import groovy.lang.Closure;
 import org.gradle.api.Task;
 import org.gradle.api.tasks.OutputFile;
 
 import java.io.File;
-import java.util.function.Supplier;
 
 public interface WarOutputTask extends Task {
     @OutputFile
     File getOutputWar();
-
-    void setOutputWar(Supplier<File> outputWar);
-
-    default void setOutputWar(Closure<File> outputWar) {
-        setOutputWar(() -> outputWar.call());
-    }
-
-    default void setOutputWar(File outputWar) {
-        setOutputWar(() -> outputWar);
-    }
-
 }
