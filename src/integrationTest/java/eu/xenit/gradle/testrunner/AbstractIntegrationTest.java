f7f19e96d85c9ff7ba26a2831cf525577ebbbf83
Lars Vierbergen <lars.vierbergen@xenit.eu>
2019-03-14 07:56:16 +0100

    Run tests with all minor gradle versions from 4.0 - 5.1

--- src/integrationTest/java/eu/xenit/gradle/testrunner/AbstractIntegrationTest.java
+++ src/integrationTest/java/eu/xenit/gradle/testrunner/AbstractIntegrationTest.java
@@ -6,17 +6,47 @@ import static org.junit.Assert.assertTrue;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Collection;
 import org.apache.commons.io.FileUtils;
 import org.gradle.testkit.runner.BuildResult;
 import org.gradle.testkit.runner.GradleRunner;
 import org.gradle.testkit.runner.TaskOutcome;
 import org.junit.Rule;
 import org.junit.rules.TemporaryFolder;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
 
 /**
  * Created by thijs on 3/2/17.
  */
+@RunWith(Parameterized.class)
 public abstract class AbstractIntegrationTest {
+
+    @Parameter(0)
+    public String gradleVersion;
+
+    @Parameters(name = "Gradle v{0}")
+    public static Collection<Object[]> testData() {
+        return Arrays.asList(new Object[][]{
+                {"5.1.1"},
+                {"5.0"},
+                {"4.10.3"},
+                {"4.9"},
+                {"4.8.1"},
+                {"4.7"},
+                {"4.6"},
+                {"4.5.1"},
+                {"4.4.1"},
+                {"4.3.1"},
+                {"4.2"},
+                {"4.1"},
+                {"4.0.2"},
+        });
+    }
+
 
     protected void testProjectFolderThatShouldFail(Path projectFolder, String task) throws IOException {
         testProjectFolder(projectFolder, task, true);
@@ -27,7 +57,7 @@ public abstract class AbstractIntegrationTest {
     }
 
     protected void testProjectFolder(Path projectFolder, String task, boolean expectsException) throws IOException {
-        if(expectsException) {
+        if (expectsException) {
             testProjectFolderExpectFailure(projectFolder, task, null);
         } else {
             testProjectFolderExpectSucces(projectFolder, task, null);
@@ -61,12 +91,11 @@ public abstract class AbstractIntegrationTest {
         if (gitDir.exists()) {
             FileUtils.moveDirectory(gitDir, tempExample.toPath().resolve(".git").toFile());
         }
-        System.out.println("Executing test build for example " + tempExample.getName());
 
         return GradleRunner.create()
                 .withProjectDir(tempExample)
                 .withArguments(task, "--stacktrace", "--rerun-tasks")
-                .withTestKitDir(new File(System.getProperty("user.home") + File.separator + ".gradle"))
+                .withGradleVersion(gradleVersion)
                 .withPluginClasspath()
                 .withDebug(true)
                 .forwardOutput();
