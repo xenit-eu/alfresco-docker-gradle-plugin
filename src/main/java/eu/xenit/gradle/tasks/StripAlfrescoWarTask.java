d1624cf6387a2117d0ed934cd8018228ccf50c45
Lars Vierbergen <lars.vierbergen@xenit.eu>
2019-08-22 12:05:13 +0200

    Only unmount the particular truezip file that is being worked on, instead of unmounting all items

--- src/main/java/eu/xenit/gradle/tasks/StripAlfrescoWarTask.java
+++ src/main/java/eu/xenit/gradle/tasks/StripAlfrescoWarTask.java
@@ -1,20 +1,14 @@
 package eu.xenit.gradle.tasks;
 
 import de.schlichtherle.truezip.file.TFile;
-import org.alfresco.repo.module.tool.WarHelperImpl;
-import org.gradle.api.DefaultTask;
-import org.gradle.api.tasks.InputFile;
-import org.gradle.api.tasks.Internal;
-import org.gradle.api.tasks.OutputFile;
-import org.gradle.api.tasks.TaskAction;
-
-import java.io.File;
 import java.io.IOException;
 import java.io.UncheckedIOException;
-import java.util.*;
-import java.util.function.Supplier;
+import java.util.HashSet;
+import java.util.Set;
+import org.gradle.api.tasks.TaskAction;
 
 public class StripAlfrescoWarTask extends ResolveWarTask {
+
     private Set<String> pathsToCopy = new HashSet<>();
 
     public void addPathToCopy(String path) {
@@ -27,9 +21,9 @@ public class StripAlfrescoWarTask extends ResolveWarTask {
         Util.withWar(getInputWar(), inputWar -> {
             Util.withWar(getOutputWar(), outputWar -> {
                 try {
-                    for(String pathToCopy: pathsToCopy)  {
-                        TFile fileToCopy = new TFile(inputWar.getAbsolutePath()+pathToCopy);
-                        TFile fileToReceive = new TFile(outputWar.getAbsolutePath()+pathToCopy);
+                    for (String pathToCopy : pathsToCopy) {
+                        TFile fileToCopy = new TFile(inputWar.getAbsolutePath() + pathToCopy);
+                        TFile fileToReceive = new TFile(outputWar.getAbsolutePath() + pathToCopy);
                         TFile.cp(fileToCopy, fileToReceive);
                     }
                 } catch (IOException e) {
