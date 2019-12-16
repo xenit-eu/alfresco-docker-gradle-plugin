33395c861f0c5361f67b97df8d3a04b855bfb0d4
Lars Vierbergen <lars.vierbergen@xenit.eu>
2019-12-16 18:33:42 +0100

    Implement global truezip lock around all operations that use truezip.
    We think raceconditions in concurrent truezip usage result in hard to diagnose issues like #72

--- src/main/java/eu/xenit/gradle/tasks/Util.java
+++ src/main/java/eu/xenit/gradle/tasks/Util.java
@@ -13,22 +13,33 @@ import java.util.function.Consumer;
 
 class Util {
 
+    private static final Object TVFS_LOCK = new Object();
+
+    static void withGlobalTvfsLock(Runnable runnable) {
+        Objects.requireNonNull(runnable);
+        synchronized (TVFS_LOCK) {
+            runnable.run();
+        }
+    }
+
     static void withWar(File warFile, Consumer<TFile> closure) {
         Objects.requireNonNull(warFile, "warFile");
         Objects.requireNonNull(closure, "closure");
+        withGlobalTvfsLock(() -> {
             TConfig config = TConfig.get();
             config.setArchiveDetector(new TArchiveDetector("war|amp", new JarDriver(IOPoolLocator.SINGLETON)));
             TFile archive = new TFile(warFile);
-        try {
-            closure.accept(archive);
-        } finally {
             try {
-                TVFS.umount(archive);
-            } catch (FsSyncException ignored) {
-                // This exception is intentionally ignored.
-                // Throwing exceptions in a finally block would swallow the original exception
-                // And it is no big deal if the archive can not be unmounted now, it will be unmounted during process shutdown anyways
+                closure.accept(archive);
+            } finally {
+                try {
+                    TVFS.umount(archive);
+                } catch (FsSyncException ignored) {
+                    // This exception is intentionally ignored.
+                    // Throwing exceptions in a finally block would swallow the original exception
+                    // And it is no big deal if the archive can not be unmounted now, it will be unmounted during process shutdown anyways
+                }
             }
-        }
+        });
     }
 }
