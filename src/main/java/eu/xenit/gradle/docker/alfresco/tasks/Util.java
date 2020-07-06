2b0f9518dcf0d2d5be65a7f803e4c6a490171528
Lars Vierbergen <lars.vierbergen@xenit.eu>
2020-07-06 18:17:46 +0200

    Clean up truezip umount experiment code

--- src/main/java/eu/xenit/gradle/docker/alfresco/tasks/Util.java
+++ src/main/java/eu/xenit/gradle/docker/alfresco/tasks/Util.java
@@ -16,42 +16,17 @@ final class Util {
     private static final Object TVFS_LOCK = new Object();
     private static Exception tvfsUmountException = null;
 
-    private static final String PROP_UMOUNT_EXPERIMENT_BASE = "eu.xenit.gradle.docker.flags.TrueZipUmountExperiment";
-    private static final String PROP_UMOUNT_EXPERIMENT_DISABLE_UMOUNT = PROP_UMOUNT_EXPERIMENT_BASE + ".disableUmount";
-    private static final String PROP_UMOUNT_EXPERIMENT_LOGGING = PROP_UMOUNT_EXPERIMENT_BASE + ".logging";
-    private static final String PROP_UMOUNT_EXPERIMENT_IGNORE_EXCEPTION =
-            PROP_UMOUNT_EXPERIMENT_BASE + ".ignoreException";
-
-    private static boolean isEnabled(String property) {
-        return Boolean.parseBoolean(System.getProperty(property, "false"));
-    }
-
-
     private Util() {
-    }
-
-    private static void umountExperimentLog(String message) {
-        if (isEnabled(PROP_UMOUNT_EXPERIMENT_LOGGING)) {
-            long threadId = Thread.currentThread().getId();
-            System.err.println(String.format("TrueZipUmountExperiment[threadId=%d]: %s", threadId, message));
-        }
     }
 
     static void withGlobalTvfsLock(Runnable runnable) {
         Objects.requireNonNull(runnable);
-        umountExperimentLog("About to acquire global TVFS lock: " + TVFS_LOCK.toString());
         synchronized (TVFS_LOCK) {
-            umountExperimentLog("Acquired global TVFS lock: " + TVFS_LOCK.toString());
             try {
                 if (tvfsUmountException != null) {
-                    if (isEnabled(PROP_UMOUNT_EXPERIMENT_IGNORE_EXCEPTION)) {
-                        umountExperimentLog(
-                                "Would have thrown IllegalStateException because of previous umount failure");
-                    } else {
-                        throw new IllegalStateException(
-                                "A previous truezip unmount operation failed. This process (gradle daemon?) is in an undefined state and can not perform truezip operations anymore.",
-                                tvfsUmountException);
-                    }
+                    throw new IllegalStateException(
+                            "A previous truezip unmount operation failed. This process (gradle daemon?) is in an undefined state and can not perform truezip operations anymore.",
+                            tvfsUmountException);
                 }
                 runnable.run();
             } finally {
@@ -59,26 +34,16 @@ final class Util {
                     // After doing something with truezip, unmount all the filesystems.
                     // This code can be run in the gradle daemon, and truezip is keeping global caches there
                     // When files change from underneath it, it may have cached wrong information about it
-                    if (isEnabled(PROP_UMOUNT_EXPERIMENT_DISABLE_UMOUNT)) {
-                        umountExperimentLog("Not calling TVFS.umount() because umount is disabled");
-                    } else {
-                        umountExperimentLog("Calling TVFS.umount()");
-                        TVFS.umount();
-                        umountExperimentLog("Finished TVFS.umount()");
-                    }
+                    TVFS.umount();
                 } catch (FsSyncException e) {
-                    if (isEnabled(PROP_UMOUNT_EXPERIMENT_LOGGING)) {
-                        umountExperimentLog("TVFS.umount() threw exception");
-                        e.printStackTrace(System.err);
-                    }
+                    // Something went wrong during unmounting.
+                    // We can't do anything about that here. Throwing exceptions in a finally block would swallow the original exception.
+                    // We now know that unmounting TVFS has failed, so we remember that.
+                    // We throw it the next time a truezip operation is attempted. (It will likely fail with a strange message otherwise)
                     tvfsUmountException = e;
-                    // This exception is intentionally ignored.
-                    // Throwing exceptions in a finally block would swallow the original exception
                 }
             }
-            umountExperimentLog("About to release global TVFS lock: " + TVFS_LOCK.toString());
         }
-        umountExperimentLog("Released global TVFS lock: " + TVFS_LOCK.toString());
     }
 
     static void withWar(File warFile, Consumer<TFile> closure) {
